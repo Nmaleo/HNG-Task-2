f787fd3e3c7100861682731d9ca88025dacb4a84
unknown <onwuchuruba.nma@gmail.com>
2023-09-13 23:21:05 +0100

    update test script

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 let express = require("express");
 const mongoose = require("mongoose");
 let PORT = 8080;
-// let model = require("./models/model");
+
 
 let user_route = require("./routes/userRoute");
 let ec2_url = "mongodb://54.221.51.134:27017";
-let mongo_url = "mongodb://127.0.0.1:27017";
+
 let connect_mongodb = async (url) => {
-  let conn = await mongoose.connect(url, { dbName: "api_test" });
+  let conn = await mongoose.connect(url, { dbName: "HNG_task-2" });
   console.log("successfully connected to mongodb");
 };
 connect_mongodb(ec2_url);
 
-let server = express();
+const server = express();
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
